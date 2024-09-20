760e55a525c49cd2fdec18aec697c4e86e2509fb
HamzaAhmad-098 <jhaji1223@gmail.com>
2024-09-20 10:41:18 +0500

    weeek 03

--- week 03/sum.cpp
+++ week 03/sum.cpp
@@ -3,20 +3,20 @@ using namespace std;
 main()
 {
 int no , sum;
-cout<<"enter first no.:";
+cout<<"enter first no. :";
 cin>>no;
-sum=sum+no;
-cout<<"enter second no.:";
+sum=no;
+cout<<"enter second no. :";
 cin>>no;
 sum=sum+no;
 cout<<"enter third no.:";
 cin>>no;
 sum=sum+no;
-cout<<"enter fourth no.:";
+cout<<"enter fourth no.";
 cin>>no;
 sum=sum+no;
 cout<<"enter fifth no.:";
 cin>>no;
 sum=sum+no;
-cout<<"sum is :"<<sum;
+cout<<"sum is "<<sum;
 }

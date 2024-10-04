cca83d4a12356c6e63cc20c51bf6bd711349ce8b
HamzaAhmad-098 <jhaji1223@gmail.com>
2024-10-04 10:01:33 +0500

    week 04

---
+++ week 05/game.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+void printMaze();
+void printEnemy();
+void eraseEnemy();
+void printPlayer();
+void gotoxy(int , int);
+int eX=2 , eY=2;
+int pX=2 . pY=2;
+main()
+{
+   printMaze();
+   while(x>=20);
+     {
+        printEnemy();
+        eraseEnemy();
+        if(eX>=20)
+           {
+              eX=2;
+           }
+     }
+}
+void printMaze()
+{
+         cout<<"##############################"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"#                            #"<<endl;
+         cout<<"##############################"<<endl;
+}
+void printPlayer()
+{
+  cout<<"        //\\              "; gotoxy(pX,pY);
+  cout<<"       //  \\             ";gotoxy(pX,pY+1);
+  cout<<"    ////    \\\\          ";gotoxy(pX,pY+2);
+  cout<<"  /////      \\\\\        ";gotoxy(pX,pY+3);
+  cout<<"    ||||||||||||          ";gotoxy(pX,pY+4);
+  cout<<"      ||||||||            ";gotoxy(pX,pY+5);
+  cout<<"       ~~~~~~             ";gotoxy(pX,pY+6);
+  cout<<"        !!!!              ";gotoxy(pX,pY+7);
+  cout<<"                          ";gotoxy(pX,pY+8);
+}
+
+void printEnemy()
+{
+  cout<<"          !!!!             ";gotoxy(eX,eY);
+  cout<<"         ~~~~~~            ";gotoxy(eX,eY+1);
+  cout<<"        ||||||||           ";gotoxy(eX,eY+2);
+  cout<<"      ||||||||||||         ";  gotoxy(eX,eY+3);
+  cout<<"    /////      \\\\\       ";gotoxy(eX,eY+4);
+  cout<<"      ////    \\\\         "; gotoxy(eX,eY+5);
+  cout<<"         //  \\            ";gotoxy(eX,eY+6);
+  cout<<"          //\\             ";gotoxy(eX,eY+7);
+}
+void eraseEnemy()
+{
+  cout<<"                           ";gotoxy(eX,eY);
+  cout<<"                           ";gotoxy(eX,eY+1);
+  cout<<"                           ";gotoxy(eX,eY+2);
+  cout<<"                           ";  gotoxy(eX,eY+3);
+  cout<<"                           ";gotoxy(eX,eY+4);
+  cout<<"                           "; gotoxy(eX,eY+5);
+  cout<<"                           ";gotoxy(eX,eY+6);
+  cout<<"                           ";gotoxy(eX,eY+7);
+}
+void gotoxy(eX ,eY)
+
+{
+
+COORD coordinates;
+
+coordinates.X = x;
+
+coordinates.Y = y;
+
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+
+}
---
+++ week 05/pyramid.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int pyramid();
+string unit;
+double length ,  width ,  height;
+main()
+  {
+   
+    
+    cout<<"enter the length :";
+    cin>> length;
+    cout<<"enter the width :";
+    cin>>width;
+    cout<<"enter the height :";
+    cin>>height;
+    cout<<"enter your desired unit (centimeter , millimeter ,kilometers) : ";
+    cin>> unit;
+    pyramid ();
+  }
+ int pyramid()
+  {
+    double volumne ;
+    volumne = (1/3)*length*height*width;
+    if(unit=="centimeter")
+        {
+           volumne = volumne/(10*10);
+           cout<<"volume is  "<<volumne<<" cubic "<<unit;
+        } 
+    if(unit=="millimeter")
+        {
+          volumne = volumne/(10*10*10);
+          cout<<"volumne is   "<<volumne<<" cubic"<<unit;
+        }
+    if(unit=="kilometers")
+        {
+          volumne = volumne*10*10*10;
+          cout<<"volumne is  "<<volumne<<" cubic "<<unit;
+        }
+    return volumne;
+  }
